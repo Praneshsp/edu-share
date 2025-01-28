2c8d4fe69b74b25ca91454af29de29dbd11236ed
sn0122 <sn0122@srmist.edu.in>
2025-01-28 16:32:47 +0530

    feat: added supabase setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,8 @@
-'use client'
-import Image from "next/image";
+import { createClient } from '@/utils/supabase/server';
 
-export default function Home() {
-  return (
-    <button className="px-6 py-2 border border-blue-400 hover:border-blue-600 shadow-md rounded-full m-3 hover:scale-100 transition-all duration-150"  
-    onClick={() => alert("Pranesh Got 10 CGPA")}
-    >
-      Hello Pranesh Asda
-    </button>
-  );
-
-
-  /*
-  div {
-  color : black
-  }
-  */
+export default async function Page() {
+  const supabase = await createClient();
+  const { data: countries } = await supabase.from("countries").select();
+  
+  return <pre>{JSON.stringify(countries, null, 2)}</pre>
 }
